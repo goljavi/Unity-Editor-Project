145a3471f6446730bee374266a614541e2f8d967
Marcos <marcos.biagi@gmail.com>
2018-10-07 01:39:00 -0300

    initial commit. nodos

---
+++ Assets/Node.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node {
+
+    public string textContent;
+    public Node parent;
+    public List<Node> children;
+
+    public Node(string newTextContent, Node newParent) //Crear nodo hijo o comun
+    {
+        textContent = newTextContent;
+        parent = newParent;
+    }
+
+    public Node(string newTextContent) // Crear nodo raiz o inicial
+    {
+        textContent = newTextContent;
+    }
+
+    public void CreateChild(string newTextContent)
+    {
+        Node child = new Node(newTextContent, this);
+    }
+
+}
