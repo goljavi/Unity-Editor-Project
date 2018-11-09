cc4018589eada992347c3502bd9a9c2b31c59321
Marcos <marcos.biagi@gmail.com>
2018-11-09 02:25:41 -0300

    initial branch commit

---
+++ Assets/InteracitonsCounter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteracitonsCounter : MonoBehaviour {
+
+    public float interactions = 0;
+    //REFERENCE DIALOGUE SOMEHOW
+
+    void IncreaseInteractionCounter()
+    {
+        interactions++;
+        //UPDATE DIALOGUE PARAMETERS
+    }
+}
